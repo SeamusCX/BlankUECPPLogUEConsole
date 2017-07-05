f0d33f48535242672162164015c7c57404521c9e
Seamus <2052529100@qq.com>
2017-07-05 16:48:42 +0800

    添加：Uobject 类 UserProfile
    添加：Uobject 类 UserProfile 啊啊啊

---
+++ Source/BlankUECPP/UserProfile.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+#include "UserProfile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLANKUECPP_API UUserProfile : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
