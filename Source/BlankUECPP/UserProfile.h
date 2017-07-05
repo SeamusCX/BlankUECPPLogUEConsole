e41d2a6e38938cce8a38762116b7cc92056160ad
Seamus <2052529100@qq.com>
2017-07-05 20:20:06 +0800

    修改：UserProfile 功能完成
    定义一个自定义类，添加到Uclass（），自己控制生成与销毁
    添加三个属性暴露给蓝图

--- Source/BlankUECPP/UserProfile.h
+++ Source/BlankUECPP/UserProfile.h
@@ -8,12 +8,53 @@
 /**
  * 
  */
-UCLASS()
+class FDataStorage
+{
+
+public:
+	FDataStorage()
+	{
+		RawData = new int32[3]{ 1,2,3 };
+	}
+	~FDataStorage()
+	{
+		if (RawData)
+		{
+			delete[] RawData;
+		}
+	}
+private:
+	
+	int32 *RawData;
+
+};
+
+
+
+
+
+UCLASS( Blueprintable )
 class BLANKUECPP_API UUserProfile : public UObject
 {
 	GENERATED_BODY()
+		UUserProfile();
 	
+public:
+	virtual void BeginDestroy() override;
+
+	//定义一个最大生命值
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = UserData)
+		float HpMax;
+	//定义一个护照值
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = UserData)
+		float Armor;
+	//定义一个玩家的颜色，可见但是不能改，蓝图里无法Get、Set，默认折叠不显示的
+	UPROPERTY(VisibleAnywhere, AdvancedDisplay, Category = Userdata)
+		FColor PlayerColor;
 	
-	
-	
+
+private:
+	FDataStorage *DataStorage;
+
+
 };
